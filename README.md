e2fba0b5da527c38418db9f51a600d3c657f43fb
Kurt Wolf <kurtwolfbuilds@gmail.com>
2022-11-19 12:05:11 -0500

    readme

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Usage
+
+    $ extract /path postgres://user:pass@host:port/db
+    /db
+
+    $ extract path postgres://user:pass@host:port/db
+    db
+
+# Installation
+
+    git clone https://github.com/kurtbuilds/extract
+    just install
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand, ValueEnum};
+use anyhow::{Result, anyhow};
+use url::Url;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Cli {
+    extract_command: String,
+    target: String,
+}
+
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    let output = cli.extract_command;
+    let url = Url::parse(&cli.target)?;
+
+    let output = output.replace("path", &url.path()[1..]);
+    println!("{}", output);
+    Ok(())
 }
